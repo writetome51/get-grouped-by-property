b891f64090e3c836880694a73b373a83c01bc0d3
Steve Thompson <writetome51@gmail.com>
2019-02-11 20:34:57 -0700

    added more tests

--- tests.js
+++ tests.js
@@ -15,6 +15,26 @@ if (arrays_match_1.arraysMatch(groups, [
     console.log('test 1 passed');
 else
     console.log('test 1 FAILED');
-var objs = [{ prop: 'the' }, { prop: '1.0000111', a: 1 }, { prop: 2.0 }, { prop: '2' }, { prop: 'THE' }, { prop: 1.00001111, a: 3 }];
+var objs = [{ prop: 'the' }, { prop: '1.00001111' }, { prop: 2.0 }, { prop: '2' }, { prop: 'THE' }, { prop: 1.00001111 }];
 groups = index_1.getGroupedByProperty('prop', objs);
-console.log(groups);
+if (groups[0][0].prop === '1.00001111' && groups[0][1].prop === 1.00001111
+    && groups[1][0].prop === 2 && groups[1][1].prop === '2'
+    && groups[2][0].prop === 'THE' && groups[2][1].prop === 'the')
+    console.log('test 2 passed');
+else
+    console.log('test 2 FAILED');
+objs = [
+    { person: { hair: 'red', name: 'tom' } },
+    { person: { hair: 'brown', name: 'ron' } },
+    { person: { hair: 'black', name: 'harry' } },
+    { person: { hair: 'blue', name: 'barry' } },
+    { person: { hair: 'brown', name: 'midge' } },
+    { person: { hair: 'black', name: 'sandy' } }
+];
+groups = index_1.getGroupedByProperty('person.hair', objs);
+if (groups[0][0].person.hair === 'black' && groups[0][1].person.hair === 'black'
+    && groups[1].length === 1 && groups[1][0].person.hair === 'blue' && groups[2][0].person.hair === 'brown'
+    && groups[2][1].person.hair === 'brown' && groups[3].length === 1 && groups[3][0].person.hair === 'red')
+    console.log('test 3 passed');
+else
+    console.log('test 3 FAILED');
